e1e333d45d0db07c6c9fdf922f0a860dbfd31207
Søren Bruus Zeppelin <soerenbf@gmail.com>
2025-01-08 15:00:04 +0100

    Add userspace for shared code

---
+++ users/soerenbf/config.h
@@ -0,0 +1,10 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+
+#undef TAPPING_TERM
+#define TAPPING_TERM 250
+
+#define PERMISSIVE_HOLD
+
---
+++ users/soerenbf/rules.mk
@@ -0,0 +1,5 @@
+CAPS_WORD_ENABLE = yes
+
+# Custom QMK stuff
+SRC += features/achordion.c
+SRC += features/custom_shift_keys.c

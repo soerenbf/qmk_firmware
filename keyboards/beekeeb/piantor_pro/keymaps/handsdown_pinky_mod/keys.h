91e7928edea2726c93e1f88416ef28bf134c05f3
Søren Bruus Zeppelin <soerenbf@gmail.com>
2025-01-28 09:27:35 +0100

    pinky mod back to 'aeih'

--- keyboards/beekeeb/piantor_pro/keymaps/handsdown_pinky_mod/keys.h
+++ keyboards/beekeeb/piantor_pro/keymaps/handsdown_pinky_mod/keys.h
@@ -7,10 +7,10 @@
 #define HM_F RALT_T(KC_F)
 
 // RH mods
-#define HM_H RSFT_T(KC_H)
-#define HM_A RCTL_T(KC_A)
-#define HM_E LALT_T(KC_E)
-#define HM_I RGUI_T(KC_I)
+#define HM_A RSFT_T(KC_A)
+#define HM_E RCTL_T(KC_E)
+#define HM_I LALT_T(KC_I)
+#define HM_H RGUI_T(KC_H)
 
 #define HM_DOT RALT_T(KC_DOT)
 
@@ -25,3 +25,6 @@
 #define MC_CPY LGUI(KC_C)
 #define MC_PST LGUI(KC_V)
 #define MC_CUT LGUI(KC_X)
+
+// vi
+#define VI_JMP LCTL(KC_CIRC)

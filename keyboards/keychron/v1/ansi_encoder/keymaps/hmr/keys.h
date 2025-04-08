1ae05caa0c7e4deae64a92cf11fb510cfdd2275c
Søren Bruus Zeppelin <soerenbf@gmail.com>
2025-04-08 08:55:13 +0200

    Finish implementation of v1/2 HMR/BMR

---
+++ keyboards/keychron/v1/ansi_encoder/keymaps/hmr/keys.h
@@ -0,0 +1,32 @@
+// LH mods (home row)
+#define HM_A LGUI_T(KC_A)
+#define HM_S LALT_T(KC_S)
+#define HM_D LCTL_T(KC_D)
+#define HM_F LSFT_T(KC_F)
+
+// #define HM_X RALT_T(KC_X)
+
+// RH mods (bottom row)
+#define HM_M RSFT_T(KC_M)
+#define HM_COMM RCTL_T(KC_COMM)
+#define HM_DOT LALT_T(KC_DOT)
+#define HM_SLSH RGUI_T(KC_SLSH)
+
+// #define HM_DOT RALT_T(KC_DOT)
+
+// Layers
+#define LT_SPC LT(2,KC_SPC) // 2 = nav
+#define LT_LCMD LT(3,KC_LCMD) // 3 = sym
+#define LT_LALT LT(3,KC_LALT) // 3 = sym
+
+// MacOS
+#define MC_CPY LGUI(KC_C)
+#define MC_PST LGUI(KC_V)
+#define MC_CUT LGUI(KC_X)
+
+// vi
+#define VI_JMP  LCTL(KC_CIRC)
+#define VI_IN   LCTL(KC_I)
+#define VI_OUT  LCTL(KC_O)
+#define VI_DOWN LCTL(KC_D)
+#define VI_UP   LCTL(KC_U)
